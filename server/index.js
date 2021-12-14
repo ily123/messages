d0cfce2c61ecfe6757740313f23a3a88d216e941
ily123 <ibnovikov@gmail.com>
2021-12-14 15:41:29 -0600

    Set up proxy forwarding.

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 app = express();
 
-app.get('/', (req, res) => {
-  res.send("hello, this is a stub")
+app.get('/api/', (req, res) => {
+  const msg = "hello, this is a stub"
+  res.json({ msg })
 })
 
 app.listen(5000, () => console.log("server started"))
