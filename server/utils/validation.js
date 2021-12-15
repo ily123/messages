8f4cd119cd081da019c78aa57c95ebea453f5143
ily123 <ibnovikov@gmail.com>
2021-12-15 13:20:55 -0600

    Refactored error reporting a bit.

--- server/utils/validation.js
+++ server/utils/validation.js
@@ -4,18 +4,19 @@ const { validationResult } = require('express-validator')
 const handleValidationErrors = (req, res, next) => {
   const validationErrors = validationResult(req)
 
-  if (!validationErrors.isEmpty()) {
-    const errors = validationErrors
-      .array()
-      .map((error) => `${error.msg}`)
+  if (validationErrors.isEmpty()) next()
 
-    const err = Error('Bad request.')
-    err.errors = errors
-    err.status = 400
-    err.title = 'Bad request.'
-    next(err)
-  }
-  next()
+  const errors = validationErrors
+    .array()
+    .map((error) => `${error.msg}`)
+  console.log('🏀 Hit request validator errors (happens before route is hit)')
+  console.log(errors)
+
+  const err = Error('Missing or innaccurate data sent with the request.')
+  err.errors = errors
+  err.status = 400
+  err.title = 'Missing or innaccurate data sent with the request.'
+  next(err)
 }
 
 module.exports = {
