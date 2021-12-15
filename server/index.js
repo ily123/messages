3904b450bff121a1a96c812c31f1529aba5a7935
ily123 <ibnovikov@gmail.com>
2021-12-14 21:16:20 -0600

    Added middle ware to app index.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const config = require('./config')
-
-console.log(config)
+const cookieParser = require('cookie-parser')
+const csurf = require('csurf')
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
 
 const app = express()
+
+app.use(cookieParser())
+app.use(express.json())
+
+const isProduction = config.environment === 'production'
+if (!isProduction) app.use(cors())
+app.use(csurf({
+  cookie: {
+    secure: isProduction,
+    sameSite: isProduction && 'Lax',
+    httpOnly: true
+  }
+}))
+app.use(helmet({ contentSecurityPolicy: false }))
+
+app.use(morgan('tiny'))
 
 app.get('/api/', (req, res) => {
   const msg = 'hello, this is a stub'
