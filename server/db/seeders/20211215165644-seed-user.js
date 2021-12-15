c6bd6475d3c2b99fa6fa0eabf2487f7f82a1fca4
ily123 <ibnovikov@gmail.com>
2021-12-15 11:00:38 -0600

    Added user seeder.

---
+++ server/db/seeders/20211215165644-seed-user.js
@@ -0,0 +1,32 @@
+'use strict'
+const faker = require('faker')
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'joe@aol.com',
+        username: 'joseph',
+        hashedPassword: bcrypt.hashSync('12345')
+      },
+      {
+        email: faker.internet.email(),
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync(faker.internet.password())
+      },
+      {
+        email: faker.internet.email(),
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync(faker.internet.password())
+      }
+    ], {})
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {})
+  }
+}
