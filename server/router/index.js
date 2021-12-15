a3930a81622fc6dd86d50340b7a0dea48b6ed66c
ily123 <ibnovikov@gmail.com>
2021-12-15 13:01:46 -0600

    Re-added XSRF cookie setter to api/ get.

--- server/router/index.js
+++ server/router/index.js
@@ -5,7 +5,8 @@ const sessionRoutes = require('./session')
 router.use('/auth', sessionRoutes)
 
 router.get('/', (req, res) => {
-  return res.json('this is a test')
+  res.cookie('XSRF-TOKEN', req.csrfToken())
+  return res.send('THIS IS A TEST, SET XSRF TOKEN')
 })
 
 module.exports = router
