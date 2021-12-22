6bf2d23ca21eb72ea20375db5b67863851fcaf45
ily123 <ibnovikov@gmail.com>
2021-12-22 14:34:59 -0600

    Stubbed out Chat.

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -4,10 +4,35 @@ export default function Chat () {
   return (
     <div className={styles.chatWrapper}>
       <Header />
+      <MessageLog />
+      <MessageEntryBox />
     </div>
   )
 }
 
 function Header () {
   return <h2>this is chat header</h2>
+}
+
+function MessageLog () {
+  return (
+    <div>
+      {[1, 2, 3, 4, 5].map(item => {
+        return <Message key={item} />
+      })}
+    </div>
+  )
+}
+
+function Message () {
+  return <p>this is a dummy message</p>
+}
+
+function MessageEntryBox () {
+  return (
+    <div>
+      <input type='textarea' />
+      <button>Submit message</button>
+    </div>
+  )
 }
