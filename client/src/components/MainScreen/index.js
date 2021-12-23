bb1d43f418e897f1613fdc9c473b6b6aed401c3b
ily123 <ibnovikov@gmail.com>
2021-12-22 23:17:01 -0600

    Added experimental useEffect to fetch messages given server id.

--- client/src/components/MainScreen/index.js
+++ client/src/components/MainScreen/index.js
@@ -2,8 +2,23 @@ import styles from './MainScreen.module.css'
 import NavBar from '../NavBar'
 import SideBar from '../SideBar'
 import Chat from '../Chat'
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { csrfFetch } from '../../store/csrf'
 
 export default function MainScreen () {
+  const { serverId } = useParams()
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(async () => {
+    const response = await csrfFetch('/api/channel/1')
+    const data = await response.json()
+    setLoaded(true)
+    console.log('😃', data)
+  }, [serverId])
+  console.log(serverId)
+
+  if (!loaded) return <div style={{ backgroundColor: 'blue' }}>...LOADING...</div>
   return (
     <div>
       <NavBar />
