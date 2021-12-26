7e34fb11698e87a156901c19d504504832a6bcee
ily123 <ibnovikov@gmail.com>
2021-12-25 18:52:30 -0600

    Added route protection.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import './App.css'
 
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { restoreUser } from './store/session'
 import LoginForm from './components/LoginForm'
 import SignupForm from './components/SignUpForm'
@@ -15,7 +15,6 @@ function App () {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(restoreUser())
-    console.log('I rendered!!')
   }, [dispatch])
 
   return (
@@ -25,12 +24,20 @@ function App () {
           <Route path='/' exact element={<SplashPage />} />
           <Route path='/login' exact element={<LoginForm />} />
           <Route path='/signup' exact element={<SignupForm />} />
-          <Route path='/main/server' exact element={<MainScreen />} />
-          <Route path='/main/server/:serverId' exact element={<MainScreen />} />
+          <Route path='/main/server' exact element={<Protected> <MainScreen /> </Protected>} />
+          <Route path='/main/server/:serverId' exact element={<Protected> <MainScreen /> </Protected>} />
+          <Route path='*' element={<div>ERROR 404: PAGE WITH THIS URL DOES NOT EXIST</div>} />
         </Routes>
       </BrowserRouter>
     </div>
   )
+}
+
+// router v6 implementaion gleaned here:
+// https://dev.to/iamandrewluca/private-route-in-react-router-v6-lg5
+function Protected ({ children }) {
+  const auth = useSelector(state => state.session)
+  return auth ? children : <Navigate to='/login' />
 }
 
 export default App
