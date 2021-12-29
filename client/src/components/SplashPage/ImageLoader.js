978a7a477f1baa8460f5b1aba206be5e107b4f09
ily123 <ibnovikov@gmail.com>
2021-12-29 17:29:18 -0600

    Added finished product features grid.
    White space looks wonky on some of the images, because the images are
    heterogeneous. I may look into replacing some of the pics later.

---
+++ client/src/components/SplashPage/ImageLoader.js
@@ -0,0 +1,19 @@
+// this is a bit dorky, but it works
+import { ReactComponent as LiveChatImg } from './undraw_chatting_re_j55r.svg'
+import { ReactComponent as WorkSpacesImg } from './undraw_duplicate_re_d39g.svg'
+import { ReactComponent as ChannelsImg } from './undraw_group_chat_re_frmo.svg'
+import { ReactComponent as DMImg } from './undraw_team_chat_re_vbq1.svg'
+import { ReactComponent as HistoryImage } from './undraw_file_searching_re_3evy.svg'
+import { ReactComponent as FreeImage } from './undraw_crypto_portfolio_2jy5.svg'
+
+export default function Image ({ id, className }) {
+  switch (id) {
+    case 1: return <LiveChatImg className={className} />
+    case 2: return <WorkSpacesImg className={className} />
+    case 3: return <ChannelsImg className={className} />
+    case 4: return <DMImg className={className} />
+    case 5: return <HistoryImage className={className} />
+    case 6: return <FreeImage className={className} />
+    default: return <LiveChatImg className={className} />
+  }
+}
--- client/src/components/SplashPage/index.js
+++ client/src/components/SplashPage/index.js
@@ -1,7 +1,6 @@
 import styles from './index.module.css'
-import SignupForm from '../SignUpForm'
-import LoginForm from '../LoginForm'
 import { NavLink } from 'react-router-dom'
+import Image from './ImageLoader'
 
 export default function SplashPage () {
   console.log(styles)
@@ -37,30 +36,51 @@ function HeroImage () {
 }
 
 function ProductDetails () {
-  const f = {
-    id: 1,
-    title: 'FREE',
-    descripton: 'All features free, and you can invest the extra skrilla into NFTs'
-  }
-  const x = [f, f, f, f, f, f]
+  const features = [
+    {
+      id: 1,
+      title: 'LIVE CHAT',
+      descripton: 'The WS library powers lightning-fast instant messaging'
+    }, {
+      id: 2,
+      title: 'WORK SPACES',
+      descripton: 'Create an unlimited number of workspaces & servers'
+    }, {
+      id: 3,
+      title: 'CHANNELS',
+      descripton: 'Customize your workspace with topic-specific chat channels'
+    }, {
+      id: 4,
+      title: 'DMs',
+      descripton: 'Send direct messages to anyone on the same server'
+    }, {
+      id: 5,
+      title: 'HISTORY',
+      descripton: 'All message history is accessible in perpetuity without a paywall'
+    }, {
+      id: 6,
+      title: 'FREE',
+      descripton: 'All features are free, and you can invest the extra skrilla into NFTs'
+    }
+  ]
   return (
     <div className={styles.productDetailsWrapper}>
       <h2>
         Messages is a websocket-powered instant chat messenger
       </h2>
       <div className={styles.detailCardsGrid}>
-        {x.map(item => <DetailsCard key={`item-${item.id}`} details={item} />)}
+        {features.map(item => <DetailsCard key={`item-${item.id}`} details={item} />)}
       </div>
     </div>
   )
 }
 
 function DetailsCard ({ details }) {
-  const { title, descripton } = details
+  const { title, descripton, id } = details
   return (
     <div className={styles.detailsCard}>
       <h3 className={styles.cardTitle}>{title}</h3>
-      <div className={styles.cardImage} />
+      <Image className={styles.cardImage} id={id} />
       <div className={styles.cardDescription}>{descripton}</div>
     </div>
   )
