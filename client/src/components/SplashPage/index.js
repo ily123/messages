6e10fefb32e2ebb17f70b14094ccc06bcef0ef44
ily123 <ibnovikov@gmail.com>
2021-12-29 14:24:50 -0600

    Started work on the final SplashPage.

--- client/src/components/SplashPage/index.js
+++ client/src/components/SplashPage/index.js
@@ -1,18 +1,36 @@
+import styles from './index.module.css'
 import SignupForm from '../SignUpForm'
 import LoginForm from '../LoginForm'
+import { NavLink } from 'react-router-dom'
 
 export default function SplashPage () {
+  console.log(styles)
   return (
-    <>
-      <div style={{
-        backgroundColor: 'black',
-        padding: '100px',
-        color: 'white'
-      }}
-      >🐤🐤🐤 There will be a splash page here.
+    <div className={styles.splashPage}>
+      <SplashNavBar />
+      <HeroImage />
+    </div>
+  )
+}
+
+function SplashNavBar () {
+  return (
+    <nav>
+      <div>LOGO?</div>
+      <div className={styles.navLinks}>
+        <NavLink to='/login'>Log in</NavLink>
+        <NavLink to='/signup'>Sign up</NavLink>
       </div>
-      <SignupForm />
-      <LoginForm />
-    </>
+    </nav>
+  )
+}
+
+function HeroImage () {
+  return (
+    <div className={styles.heroImage}>
+      <div className={styles.centerLogo} />
+      <h1>Messages by Ilya</h1>
+      <h2>when Telegram, Slack, LINE, Viber, Discord, and ICQ aren't enough </h2>
+    </div>
   )
 }
