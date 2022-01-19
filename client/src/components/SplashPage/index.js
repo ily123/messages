e49164b684b5578f49f155a96d079315c15ce823
ily123 <ibnovikov@gmail.com>
2022-01-19 12:39:22 -0600

    Broke splash landing strings into paragraphs.

--- client/src/components/SplashPage/index.js
+++ client/src/components/SplashPage/index.js
@@ -46,27 +46,27 @@ function ProductDetails () {
     {
       id: 1,
       title: 'LIVE CHAT',
-      descripton: 'The WS library powers lightning-fast instant messaging'
+      descripton: ['The WS library powers', 'lightning-fast instant messaging']
     }, {
       id: 2,
       title: 'WORK SPACES',
-      descripton: 'Create an unlimited number of workspaces & servers'
+      descripton: ['Create an unlimited number of', 'workspaces & servers']
     }, {
       id: 3,
       title: 'CHANNELS',
-      descripton: 'Customize your workspace with topic-specific chat channels'
+      descripton: ['Customize your workspace with', 'topic-specific chat channels']
     }, {
       id: 4,
       title: 'DMs',
-      descripton: 'Send direct messages to anyone on the same server'
+      descripton: ['Send direct messages to anyone', 'on the same server']
     }, {
       id: 5,
       title: 'HISTORY',
-      descripton: 'All message history is accessible in perpetuity without a paywall'
+      descripton: ['All message history is accessible in perpetuity without a paywall']
     }, {
       id: 6,
       title: 'FREE',
-      descripton: 'All features are free, and you can invest you skrilla into NFTs'
+      descripton: ['All features are free, and you can', 'invest you skrilla into NFTs']
     }
   ]
   return (
@@ -87,7 +87,9 @@ function DetailsCard ({ details }) {
     <div className={styles.detailsCard}>
       <h3 className={styles.cardTitle}>{title}</h3>
       <Image className={styles.cardImage} id={id} />
-      <div className={styles.cardDescription}>{descripton}</div>
+      <div className={styles.cardDescription}>
+        {descripton.map((line, index) => <p key={index}>{line}</p>)}
+      </div>
     </div>
   )
 }
