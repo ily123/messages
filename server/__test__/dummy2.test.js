745613a22a80dcf757b740499e2a15d9e2f15e43
Ilya Novikov <ibnovikov@gmail.com>
2022-02-08 02:09:36 -0600

    Experimenting with jest bail & coverage.

--- server/__test__/dummy2.test.js
+++ server/__test__/dummy2.test.js
@@ -1,6 +1,6 @@
-describe('This is a dummy test', function () {
+describe('This is a dummy test BAIL TEST', function () {
   test('test 1', () => {
-    expect(1).toEqual(2)
+    expect(1).toEqual(1)
   })
   test('test 2', () => {
     expect(1).toEqual(1)
